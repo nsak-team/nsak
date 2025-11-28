6bcf2f398e534162c41d8830d8f43400d5b3e84a
Lukas von Allmen <lukaschristian.vonallmen@students.bfh.ch>
2025-11-28 23:06:50 +0100

    Implements scenario loader

--- src/core/scenario/__init__.py
+++ src/core/scenario/__init__.py
@@ -1,3 +1,4 @@
 from .scenario import Scenario, ScenarioDependencies, ScenarioInterface
+from .scenario_loader import ScenarioLoader
 
-__all__ = ["Scenario", "ScenarioDependencies", "ScenarioInterface"]
+__all__ = ["Scenario", "ScenarioDependencies", "ScenarioInterface", "ScenarioLoader"]
