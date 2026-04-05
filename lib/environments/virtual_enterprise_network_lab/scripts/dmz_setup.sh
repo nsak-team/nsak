dcdf5a39c6ee45ea1127c10a2770d1fc2de4a83f
gausf1 <gausf1@bfh.ch>
2026-04-05 08:25:09 +0200

    remove omitted environments and set up simple nginx web server

--- lib/environments/virtual_enterprise_network_lab/scripts/dmz_setup.sh
+++ lib/environments/virtual_enterprise_network_lab/scripts/dmz_setup.sh
@@ -98,3 +98,43 @@ named-checkzone vlab.local /etc/bind/vlab.local.zone || { echo "[dns] zone inval
 
 # Start BIND9
 named -4 -c /etc/bind/named.conf
+
+# Set Up simple webserver
+
+#openssl certificate self signed
+openssl req -x509 -nodes -newkey rsa:2048 -days 365 \
+  -keyout /etc/ssl/lab.key \
+  -out /etc/ssl/lab.crt \
+  -subj "/CN=dmz-server.vlab.local"
+
+# Set Up nginx
+cat > /etc/nginx/http.d/default.conf << 'EOF'
+server {
+  listen 80;
+  server_name dmz-server.vlab.local 172.16.1.10;
+  return 301 https://$host$request_uri;
+}
+server {
+  listen 443 ssl;
+  server_name dmz-server.vlab.local 172.16.1.10;
+
+  ssl_certificate  /etc/ssl/lab.crt;
+  ssl_certificate_key /etc/ssl/lab.key;
+
+  location / {
+  return 200 'NSAK Enterprise';
+  add_header Content-Type text/plain;
+  }
+  location /secret.txt {
+    return 200 "Confidential\n DB Password: {flag:exposed-SOS}";
+    add_header Content-Type text/plain;
+  }
+}
+EOF
+
+nginx
+
+curl http://172.16.1.10
+curl -k https://172.16.1.10
+curl -k https://172.16.1.10/secret.txt
+
