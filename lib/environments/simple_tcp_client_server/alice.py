d7ded429e5c88b9f891e104f6793e3e197952ee4
gausf1 <gausf1@bfh.ch>
2026-04-09 18:14:51 +0200

    clean up wrong state version control

--- lib/environments/simple_tcp_client_server/alice.py
+++
@@ -1,7 +0,0 @@
-from connection import run_client
-
-BOB_HOST = "10.10.100.20"
-BOB_PORT = 5000
-
-if __name__ == "__main__":
-    run_client(BOB_HOST, BOB_PORT, label="Alice")
--- lib/environments/simple_tcp_client_server/bob.py
+++
@@ -1,7 +0,0 @@
-from connection import run_server
-
-HOST = "0.0.0.0"
-PORT = 5000
-
-if __name__ == "__main__":
-    run_server(HOST, PORT, label="Bob")
