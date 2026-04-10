14c4af8b19a01478d530044255efc6b9cc60e2aa
gausf1 <gausf1@bfh.ch>
2026-04-10 19:44:00 +0200

    resolve gateway over management network missconfiguration
    # Conflicts:
    #	lib/environments/virtual_enterprise_network_lab/virtual_enterprise_network_lab.clab.yaml

--- lib/environments/virtual_enterprise_network_lab/scripts/host-setup.sh
+++ lib/environments/virtual_enterprise_network_lab/scripts/host-setup.sh
@@ -2,3 +2,6 @@ sudo ip link add dmz-br type bridge && sudo ip link set dmz-br up
 sudo ip link add lan-br type bridge && sudo ip link set lan-br up
 sudo ip link add wan-br type bridge && sudo ip link set wan-br up
 
+sudo ip addr add 10.0.1.254/24 dev wan-br
+
+sudo iptables -t nat -A POSTROUTING -s 10.0.1.0/24 ! -d 10.0.1.0/24 -j MASQUERADE
