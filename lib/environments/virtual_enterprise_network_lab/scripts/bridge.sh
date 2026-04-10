ecc0829a8b543d4adaba1191b8809963e103cdd3
Lukas von Allmen <lukaschristian.vonallmen@students.bfh.ch>
2026-04-10 15:33:51 +0200

    Adds run/.gitignore to repo

--- lib/environments/virtual_enterprise_network_lab/scripts/bridge.sh
+++ lib/environments/virtual_enterprise_network_lab/scripts/bridge.sh
@@ -1,3 +1,3 @@
-sudo ip link add dmz-br type bridge && sudo ip link set dmz-br up
-sudo ip link add lan-br type bridge && sudo ip link set lan-br up
-sudo ip link add wan-br type bridge && sudo ip link set wan-br up
+sudo ip link add dmz-br type bridge && sudo ip link set dmz-br up && sudo sysctl net.ipv6.conf.dmz-br.disable_ipv6=1
+sudo ip link add lan-br type bridge && sudo ip link set lan-br up && sudo sysctl net.ipv6.conf.lan-br.disable_ipv6=1
+sudo ip link add wan-br type bridge && sudo ip link set wan-br up && sudo sysctl net.ipv6.conf.wan-br.disable_ipv6=1
